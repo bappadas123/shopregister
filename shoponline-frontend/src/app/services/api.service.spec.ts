c1ee4ae052fa3049fde0436fbc28ee4e93430b6a
bappadas123 <das_bappa2002@yahoo.co.in>
2026-07-03 22:33:25 +0530

    servie api

--- shoponline-frontend/src/app/services/api.service.spec.ts
+++ shoponline-frontend/src/app/services/api.service.spec.ts
@@ -1,16 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
-import { ApiService } from './api.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
 
-describe('ApiService', () => {
-  let service: ApiService;
+  private http = inject(HttpClient);
+  getAbout() {
+  return this.http.get('http://localhost:8080/api/about');
+}
+  getContact() {
+  return this.http.get('http://localhost:8080/api/about');
+}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+}
