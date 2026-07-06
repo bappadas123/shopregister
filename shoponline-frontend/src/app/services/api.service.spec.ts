c81b41016e3b94a251829ce890d5f291c722325e
bappadas123 <das_bappa2002@yahoo.co.in>
2026-07-06 16:29:05 +0530

    ci add

--- shoponline-frontend/src/app/services/api.service.spec.ts
+++ shoponline-frontend/src/app/services/api.service.spec.ts
@@ -7,11 +7,17 @@ import { Injectable, inject } from '@angular/core';
 export class ApiService {
 
   private http = inject(HttpClient);
+   private baseUrl = 'http://localhost:8080/api';
   getAbout() {
-  return this.http.get('http://localhost:8080/api/about');
+  //return this.http.get('http://localhost:8080/api/about');
+  return this.http.get(`${this.baseUrl}/about`, {
+      responseType: 'text'
+    });
 }
   getContact() {
-  return this.http.get('http://localhost:8080/api/about');
+     return this.http.get(`${this.baseUrl}/contact`, {
+      responseType: 'text'
+    });
 }
 
 
